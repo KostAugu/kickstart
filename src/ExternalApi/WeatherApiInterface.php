5a29ace3436ecad5c752d0373dcccb450f4499f2
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-03 18:44:03 +0300

    Class work

---
+++ src/ExternalApi/WeatherApiInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\ExternalApi;
+use App\Model\Weather;
+interface WeatherApiInterface
+{
+    /**
+     * @param \DateTime $day
+     * @return Weather
+     * @throws \Exception
+     */
+    public function getDay(\DateTime $day): Weather;
+}
---
+++ src/ExternalApi/YahooApi.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\ExternalApi;
+
+use App\Model\Weather;
+
+class YahooApi extends AbstractWeatherService implements WeatherApiInterface
+{
+    public const PROVIDER_NAME = 'Yahoo';
+    /**
+     * @param \DateTime $day
+     * @return Weather
+     * @throws \Exception
+     */
+    public function getDay(\DateTime $day): Weather
+    {
+        $today = parent::getDay($day);
+        $today->setProviderName(self::PROVIDER_NAME);
+        return $today;
+    }
+}
